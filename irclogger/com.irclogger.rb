172632dfbcf633dae5181e89fa62df6f6b72efc0
ephekt <mfrosengarten@gmail.com>
2012-02-10 18:57:20 -0800

    adding in iterations for channels and dates

--- irclogger/com.irclogger.rb
+++ irclogger/com.irclogger.rb
@@ -6,10 +6,15 @@ class Date
   end
 end
 
+def grab_page url
+  raise unless url
+  Nokogiri::HTML(Typhoeus::Request.get(url).body)
+end
+
 # Some vars we'll use throughout the script
 END_DATE = Date.today - 1
 START_DATE = Date.parse("2008-02-09")
-root_url = "http://irclogger.com/"
+ROOT_URL = "http://irclogger.com/"
 
 puts "Start/End Date: #{START_DATE.nice_format}/#{END_DATE.nice_format}"
 
@@ -18,7 +23,7 @@ channels_logged = if File.exists?("channels_monitored.csv")
   File.read("channels_monitored.csv").split(",")
 else
   puts "Grab the list of supported channels and write to disk"
-  set = Nokogiri::HTML(Typhoeus::Request.get(root_url).body).xpath("//section//ul//li//a")
+  set = grab_page(ROOT_URL).xpath("//section//ul//li//a")
   channels = set[3,set.size].map { |s| s['href'].split("/")[1] }
   File.open("channels_monitored.csv","w+") do |file|
     file.write(channels.join(','))
@@ -32,5 +37,9 @@ print "Channels monitored: #{channels_logged*' '}"
 # * fetch log from the web and parse
 # * store Date log content to file if it does not already exit
 START_DATE.upto(END_DATE) do |date|
-  puts date.nice_format
+  channels_logged.each do |channel|
+    channel_url = "#{ROOT_URL}#{channel}/#{date.nice_format}"
+    puts "Grabbing #{channel_url}"
+    #grab_page channel_url
+  end
 end
